3b43c8a5d3c734b65aa36d0dce6f0a5a34197aae
Daniel <github.com.rectify325@passmail.net>
2025-04-14 12:16:44 +0100

    Added Web scraping

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import itertools
+import bs4
+import requests
 
 class WordlistGenerator:
-    def __init__(self, words, numbers, output_file="wordlist.txt"):
+    def __init__(self, words, numbers, url, output_file="wordlist.txt"):
         self.words = [word.strip() for word in words.split(",")]
+        self.url = url.strip() if url else None
         self.numbers = [number.strip() for number in numbers.split(",")]
         self.output_file = output_file
         self.replacements = {
@@ -14,6 +17,21 @@ class WordlistGenerator:
             "l": "1",
             "t": "7"
         }
+
+    def url_to_word(self, url):
+        try:
+            response = requests.get(url)
+            soup = bs4.BeautifulSoup(response.text, "html.parser")
+            soup = soup.get_text()
+            words = set()
+            for word in soup.split():
+                if len(word.strip()) > 4:
+                    words.add(word.strip())
+            return list(words)
+        except requests.RequestException as e:
+            print(f"Error fetching URL: {e}")
+            return []
+
 
     def apply_replacements(self, word):
         replaced_words = []
@@ -56,12 +74,22 @@ class WordlistGenerator:
             for variation in variations:
                 combinations.extend(self.apply_replacements(variation)) 
 
-        with open(self.output_file, "w") as file:
+        with open(self.output_file, "w", encoding="utf-8") as file:
             file.write("\n".join(combinations))
+    
+    def main(self):
+        if self.url:
+            words_from_url = self.url_to_word(url)
+            if words_from_url:
+                self.words.extend(words_from_url)
+        self.generate_wordlist()
+        print(f"Wordlist generated and saved to {self.output_file}.")
 
 
 if __name__ == "__main__":
     words = input("Enter words separated by commas: ")
     numbers = input("Enter numbers separated by commas: ")
-    generator = WordlistGenerator(words, numbers)
-    generator.generate_wordlist()
+    url = input("Enter URL (optional): ")
+    output_file = input("Enter output file name (default: wordlist.txt): ") or "wordlist.txt"
+    generator = WordlistGenerator(words, numbers, url, output_file)
+    generator.main()
